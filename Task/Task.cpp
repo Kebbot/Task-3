128d092ea0dc0092cbddc1c336d8c76a6717b55f
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-02-20 22:27:36 +0300

    Task-3

---
+++ Task/Task.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int ned, day;
+	cout << "Введите количество дней: ";
+	cin >> day;
+	ned = day / 7;
+	day = day % 7;
+	cout << ned << " Недель/и\t" << day << " Дней/Дня";
+
+
+
+	return 0;
+}
